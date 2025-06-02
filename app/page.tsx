ae75d9d3ad1ab9301eb0cc145ee21004f1c8d5c8
Simon Schötz <simonsch.tz@gmail.com>
2025-06-02 12:28:49 +0200

    Base setup main page

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+
+const MainPage: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>Simon Schötz</h1>
+      </main>
+      <footer>
+        <p>© 2025 Simon Schötz</p>
+      </footer>
+    </div>
+  );
+};
+
+export default MainPage;
