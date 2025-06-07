6510a75a4080f905b18036f9f2b4ae75b2480b39
Simon Schötz <simonsch.tz@gmail.com>
2025-06-07 16:59:21 +0200

    set up fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { geistMono, geistSans } from '@/_fonts';
 
 export const metadata: Metadata = {
   title: 'Simon Schötz',
-  description: 'A informative website about Simon Schötz.',
+  description: `Hi! I'm Simon, welcome to my website!`,
 };
 
 export default function RootLayout({
@@ -25,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased flex flex-col items-center justify-center w-full`}
       >
         {children}
       </body>
