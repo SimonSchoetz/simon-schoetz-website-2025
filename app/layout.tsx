099fbcd8410c557766ed5ffc14fa2dd03f62c1a8
Simon Schötz <simonsch.tz@gmail.com>
2025-06-10 17:06:15 +0200

    add navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { geistMono, geistSans } from '@/_fonts';
+import { RouterItem } from '@/components';
+import { Route } from '@/enums';
 
 export const metadata: Metadata = {
   title: 'Simon Schötz',
@@ -17,6 +19,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased flex flex-col items-center justify-center w-full`}
       >
+        <header className='w-full p-16 max-sm:p-2'>
+          <nav>
+            <ul className='flex gap-4'>
+              <li>
+                <RouterItem
+                  className='font-mono'
+                  href={Route.HOME}
+                  label='Simon Schötz →'
+                />
+              </li>
+              <li>
+                <RouterItem
+                  className='font-mono'
+                  href={Route.VALUES}
+                  label='Values →'
+                />
+              </li>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
