6004159c126f22aa3e6db069f4884bceda3991a5
Simon Schötz <simonsch.tz@gmail.com>
2025-06-10 17:04:32 +0200

    adjust heading components so classNames prop does not overwrite component native styling

--- components/Headings.tsx
+++ components/Headings.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const H1: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
     <h1
-      className='text-[clamp(2.5rem,10vw,8rem)] leading-[1] uppercase'
+      className={`text-[clamp(2.5rem,10vw,8rem)] leading-[1] uppercase ${props.className}`}
       {...props}
     >
       {text}
@@ -17,7 +17,7 @@ export const H1: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
 
 export const H2: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
-    <h2 className='text-6xl uppercase' {...props}>
+    <h2 className={`text-6xl uppercase ${props.className}`} {...props}>
       {text}
     </h2>
   );
@@ -25,7 +25,7 @@ export const H2: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
 
 export const H3: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
-    <h3 className='text-2xl py-4 uppercase' {...props}>
+    <h3 className={`text-2xl py-4 uppercase ${props.className}`} {...props}>
       {text}
     </h3>
   );
