40b3772d985147d62f4507f776d0f3fba2556a7f
Simon Schötz <simonsch.tz@gmail.com>
2025-06-10 17:18:44 +0200

    fix heading className overwrite bug

--- components/Headings.tsx
+++ components/Headings.tsx
@@ -7,8 +7,8 @@ type Props = {
 export const H1: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
     <h1
-      className={`text-[clamp(2.5rem,10vw,8rem)] leading-[1] uppercase ${props.className}`}
       {...props}
+      className={`text-[clamp(2.5rem,10vw,8rem)] leading-[1] uppercase ${props.className}`}
     >
       {text}
     </h1>
@@ -17,7 +17,7 @@ export const H1: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
 
 export const H2: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
-    <h2 className={`text-6xl uppercase ${props.className}`} {...props}>
+    <h2 {...props} className={`text-6xl uppercase ${props.className}`}>
       {text}
     </h2>
   );
@@ -25,7 +25,7 @@ export const H2: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
 
 export const H3: FCProps<HeadingProps & Props> = ({ text, ...props }) => {
   return (
-    <h3 className={`text-2xl py-4 uppercase ${props.className}`} {...props}>
+    <h3 {...props} className={`text-2xl py-4 uppercase ${props.className}`}>
       {text}
     </h3>
   );
