95ac8c3923baf4cf06625dbc5ee198980557a6b9
Simon Schötz <simonsch.tz@gmail.com>
2025-06-10 18:16:01 +0200

    refactor Header component according to open/closed principle

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,19 +1,42 @@
 import { RouterItem } from '@/components';
 import { Route } from '@/enums';
 
+type RouterItem = {
+  href: Route;
+  label: string;
+};
+
+const RouterItems: RouterItem[] = [
+  {
+    href: Route.HOME,
+    label: 'Home →',
+  },
+  {
+    href: Route.PRINCIPLES,
+    label: 'Principles →',
+  },
+  {
+    href: Route.PROFESSIONAL_CAREER,
+    label: 'Professional Career →',
+  },
+  {
+    href: Route.PROJECTS,
+    label: 'Projects →',
+  },
+];
+
 export const Header = () => {
+  const renderItems = () => {
+    return RouterItems.map((item) => (
+      <li key={item.href}>
+        <RouterItem href={item.href} label={item.label} />
+      </li>
+    ));
+  };
   return (
     <header className='w-full p-16 max-sm:p-2'>
       <nav>
-        <ul className='flex gap-4'>
-          <RouterItem href={Route.HOME} label='Home →' />
-          <RouterItem href={Route.PRINCIPLES} label='Principles →' />
-          <RouterItem
-            href={Route.PROFESSIONAL_CAREER}
-            label='Professional Career →'
-          />
-          <RouterItem href={Route.PROJECTS} label='Projects →' />
-        </ul>
+        <ul className='flex gap-12 font-mono'>{renderItems()}</ul>
       </nav>
     </header>
   );
