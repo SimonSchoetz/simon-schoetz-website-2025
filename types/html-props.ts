0c66997ec1c43da9032ad0202613d791f7e3e450
Simon Schötz <simonsch.tz@gmail.com>
2025-07-07 11:46:15 +0200

    add div props type

--- types/html-props.ts
+++ types/html-props.ts
@@ -2,6 +2,8 @@ import { JSX } from 'react';
 
 export type SectionProps = JSX.IntrinsicElements['section'];
 
+export type DivProps = JSX.IntrinsicElements['div'];
+
 export type HeadingProps =
   | JSX.IntrinsicElements['h1']
   | JSX.IntrinsicElements['h2']
