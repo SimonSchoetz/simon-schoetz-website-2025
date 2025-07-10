6f936ecfa15391d91cc21a6c2652f96f0c7eb614
Simon Schötz <simonsch.tz@gmail.com>
2025-07-10 15:06:03 +0200

    set up structure for expandable card

--- app/_components/ProjectsSection.tsx
+++ app/_components/ProjectsSection.tsx
@@ -1,4 +1,4 @@
-import { ExpandableCard, H2, Section } from '@/components';
+import { ExpandableCard, H2, H4, Section } from '@/components';
 import { FCProps } from '@/types';
 
 type Props = object;
@@ -7,7 +7,36 @@ export const ProjectsSection: FCProps<Props> = ({ ...props }) => {
   return (
     <Section {...props}>
       <H2 text='Projects' className='mb-4' />
-      <ExpandableCard />
+      <ExpandableCard
+        title='Femlives'
+        subheading='Co-founder · CTO · 2024'
+        fixedContent={
+          <>
+            <div>
+              <H4 text='Project:' />
+              <p>Project description</p>
+            </div>
+            <div>
+              <H4 text='Role:' />
+              <p>Role description</p>
+            </div>
+          </>
+        }
+        expandableContent={
+          <>
+            <div>
+              <H4 text='Key Achievements:' />
+              <p>Key achievements description</p>
+            </div>
+
+            <div>
+              <H4 text='Learnings & Growth:' />
+              <p>Learnings and growth description</p>
+            </div>
+          </>
+        }
+        techStack={['React', 'Next.js', 'Tailwind CSS', 'TypeScript']}
+      />
     </Section>
   );
 };
--- components/ExpandableCard.tsx
+++ components/ExpandableCard.tsx
@@ -1,7 +1,52 @@
+'use client';
+
 import { FCProps } from '@/types';
+import { useState } from 'react';
+import { Button } from './Button';
+import { H3 } from './Headings';
 
-type Props = object;
+type Props = {
+  title: string;
+  subheading: string;
+  fixedContent: React.ReactNode;
+  expandableContent: React.ReactNode;
+  techStack: string[];
+};
 
-export const ExpandableCard: FCProps<Props> = ({ ...props }) => {
-  return <div {...props}>ExpandableCard</div>;
+export const ExpandableCard: FCProps<Props> = ({
+  fixedContent,
+  expandableContent,
+  title,
+  subheading,
+  techStack,
+  ...props
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => setIsExpanded(!isExpanded);
+
+  return (
+    <div {...props}>
+      <div>
+        <H3 text={title} />
+        <p>{subheading}</p>
+      </div>
+
+      {fixedContent}
+
+      {isExpanded && <div>{expandableContent}</div>}
+
+      <Button
+        config='defaultButton'
+        label={isExpanded ? 'Show less' : 'Show more'}
+        onClick={handleToggle}
+      />
+
+      <div className='flex flex-wrap gap-2'>
+        {techStack.map((tech) => (
+          <div key={tech}>{tech}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
