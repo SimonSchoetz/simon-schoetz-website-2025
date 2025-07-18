e198d46c0328f58cf71e952bfb6f813b073f51eb
Simon Schötz <simonsch.tz@gmail.com>
2025-07-18 19:44:03 +0200

    refactor Chip to accept classNames

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -1,14 +1,14 @@
-import { FCProps } from '@/types';
+import { FCProps, HtmlProps } from '@/types';
 
-type Props = {
+type Props = HtmlProps<'div'> & {
   label: string;
 };
 
-export const Chip: FCProps<Props> = ({ label, ...props }) => {
+export const Chip: FCProps<Props> = ({ label, className, ...props }) => {
   return (
     <div
       {...props}
-      className='bg-bg-2 rounded-full border border-fg-4 py-4 px-3 text-sm font-mono'
+      className={`bg-bg-2 rounded-full border border-fg-4 py-4 px-3 text-sm font-mono ${className}`}
     >
       {label}
     </div>
