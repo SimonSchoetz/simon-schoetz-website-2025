8bf3dcc6182d93254f321d2c5d0da1c4c6c9b7c8
Simon Schötz <simonsch.tz@gmail.com>
2025-07-22 19:19:34 +0200

    refactor navigation for smoother response

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,4 @@
-'use client';
-
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { Navigation } from './Navigation';
 
 type NavItem = {
   id: string;
@@ -32,87 +29,9 @@ const NavItems: NavItem[] = [
 ];
 
 export const Header = () => {
-  const [activeSection, setActiveSection] = useState<string>('top');
-  const [navTargetId, setNavTargetId] = useState<string>('');
-
-  const sectionIds = NavItems.map((item) => item.id);
-
-  const getCurrentSectionId = (): string => {
-    const scrollY = window.scrollY;
-    const isAtBottom =
-      window.innerHeight + scrollY >=
-      document.documentElement.scrollHeight - 10;
-    const fromBottomUpIds = sectionIds.toReversed();
-
-    if (isAtBottom) {
-      return fromBottomUpIds[0];
-    }
-
-    const navHeight = document.querySelector('header')?.offsetHeight;
-    const navOffset = navHeight ? navHeight + 40 : 100;
-
-    const activeSection = fromBottomUpIds.find((id) => {
-      const element = document.getElementById(id);
-      if (element) {
-        const rect = element.getBoundingClientRect();
-        const elementTop = rect.top + scrollY;
-        return scrollY + navOffset >= elementTop;
-      }
-    });
-
-    return activeSection || 'top';
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentSectionId = getCurrentSectionId();
-
-      if (navTargetId) {
-        const isNavigating = navTargetId !== currentSectionId;
-
-        if (isNavigating) {
-          setActiveSection(navTargetId);
-        } else {
-          setNavTargetId('');
-        }
-        return;
-      }
-
-      setActiveSection(currentSectionId);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    handleScroll(); // Check initial state
-
-    return () => window.removeEventListener('scroll', handleScroll);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sectionIds]);
-
-  const renderItems = () => {
-    return NavItems.map(({ id, label }) => (
-      <li key={id}>
-        <Link
-          href={`#${id}`}
-          className={` transition-all ${
-            activeSection === id ? 'cursor-default' : 'hover:underline'
-          }`}
-          onNavigate={() => setNavTargetId(id)}
-        >
-          {label}
-          {activeSection === id ? '' : ' →'}
-        </Link>
-      </li>
-    ));
-  };
-
   return (
     <header className='w-full max-sm:p-2 sticky top-0 z-10 mb-10'>
-      <nav className='px-16 backdrop-blur-md'>
-        <ul className='flex gap-20 text-xs font-mono py-6 border-b border-fg-4'>
-          {renderItems()}
-        </ul>
-      </nav>
+      <Navigation navItems={NavItems} />
     </header>
   );
 };
---
+++ app/_components/Navigation.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { Button } from '@/components';
+import { FCProps, HtmlProps } from '@/types';
+import { useEffect, useState } from 'react';
+
+type Props = HtmlProps<'nav'> & {
+  navItems: {
+    id: string;
+    label: string;
+  }[];
+};
+
+export const Navigation: FCProps<Props> = ({
+  navItems,
+  className = '',
+  ...props
+}) => {
+  const [activeSection, setActiveSection] = useState<string>('top');
+  const [navTargetId, setNavTargetId] = useState<string>('');
+
+  const sectionIds = navItems.map((item) => item.id);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentSectionId = getCurrentSectionId(sectionIds);
+
+      if (navTargetId) {
+        const isNavigating = navTargetId !== currentSectionId;
+
+        if (isNavigating) {
+          setActiveSection(navTargetId);
+        } else {
+          setNavTargetId('');
+        }
+        return;
+      }
+
+      setActiveSection(currentSectionId);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    handleScroll(); // Check initial state
+
+    return () => window.removeEventListener('scroll', handleScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sectionIds]);
+
+  const handleClick = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      window.history.pushState(null, '', `#${id}`);
+    }
+    setNavTargetId(id);
+  };
+
+  return (
+    <nav className={`px-16 backdrop-blur-md ${className}`} {...props}>
+      <ul className='flex gap-20 py-6 border-b border-fg-4 text-xs font-mono'>
+        {navItems.map(({ id, label }) => (
+          <li key={id}>
+            <Button
+              config='container'
+              className={`  ${
+                activeSection === id
+                  ? 'hover:cursor-default'
+                  : 'hover:underline'
+              }`}
+              onClick={() => handleClick(id)}
+            >
+              {label}
+              {activeSection === id ? '' : ' ➙'}
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const getCurrentSectionId = (sectionIds: string[]): string => {
+  const scrollY = window.scrollY;
+  const isAtBottom =
+    window.innerHeight + scrollY >= document.documentElement.scrollHeight - 10;
+  const fromBottomUpIds = sectionIds.toReversed();
+
+  if (isAtBottom) {
+    return fromBottomUpIds[0];
+  }
+
+  const navHeight = document.querySelector('header')?.offsetHeight;
+  const navOffset = navHeight ? navHeight + 40 : 100;
+
+  const activeSection = fromBottomUpIds.find((id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const elementTop = rect.top + scrollY;
+      return scrollY + navOffset >= elementTop;
+    }
+  });
+
+  return activeSection || 'top';
+};
