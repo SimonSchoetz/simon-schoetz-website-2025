a9e4842ab040c2e8e02cc5cacc929e5b723d2b48
Simon Schötz <simonsch.tz@gmail.com>
2025-07-31 18:27:58 +0200

    add background gradient for project card headings

--- components/ExpandableCard.tsx
+++ components/ExpandableCard.tsx
@@ -28,39 +28,47 @@ export const ExpandableCard: FCProps<CardContent> = ({
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div {...props} className='bg-bg-2 rounded-4xl p-8 lg:p-14'>
-      <div className='flex flex-col gap-6 lg:flex-row justify-between lg:items-center mb-10'>
+    <div {...props} className='bg-bg-2 rounded-4xl overflow-hidden'>
+      <div
+        className='flex flex-col gap-6 justify-between p-8
+          lg:flex-row lg:p-14 lg:items-center'
+        style={{
+          background: `linear-gradient(to bottom, color-mix(in srgb, ${colorVar} 56%, transparent) 10%, transparent 80%)`,
+        }}
+      >
         <H3 text={title} className='text-4xl' />
 
         <p className='uppercase lg:text-lg'>{subheading}</p>
       </div>
 
-      <div className='flex flex-col gap-10 mb-10 text-sm lg:text-base'>
-        {fixedContent}
+      <div className='p-8 lg:p-14 pt-0 lg:pt-0'>
+        <div className='flex flex-col gap-10 mb-10 text-sm lg:text-base'>
+          {fixedContent}
+        </div>
+
+        <ExpandableDiv
+          isExpanded={isExpanded}
+          className='flex flex-col gap-10 mb-10 text-sm lg:text-base'
+        >
+          {expandableContent}
+        </ExpandableDiv>
+
+        <div className='flex flex-wrap gap-4 mb-10'>
+          {chips.map((tech) => (
+            <Chip key={tech} label={tech} />
+          ))}
+        </div>
+
+        <Button
+          className={`w-full lg:w-fit`}
+          config='default'
+          label={isExpanded ? 'Show less' : 'Show more'}
+          onClick={() => setIsExpanded(!isExpanded)}
+          icon='upInCircle'
+          flipIcon={isExpanded}
+          colorVar={colorVar}
+        />
       </div>
-
-      <ExpandableDiv
-        isExpanded={isExpanded}
-        className='flex flex-col gap-10 mb-10 text-sm lg:text-base'
-      >
-        {expandableContent}
-      </ExpandableDiv>
-
-      <div className='flex flex-wrap gap-4 mb-10'>
-        {chips.map((tech) => (
-          <Chip key={tech} label={tech} />
-        ))}
-      </div>
-
-      <Button
-        className={`w-full lg:w-fit`}
-        config='default'
-        label={isExpanded ? 'Show less' : 'Show more'}
-        onClick={() => setIsExpanded(!isExpanded)}
-        icon='upInCircle'
-        flipIcon={isExpanded}
-        colorVar={colorVar}
-      />
     </div>
   );
 };
