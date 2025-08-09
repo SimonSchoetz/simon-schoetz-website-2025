ae32f5e7f4878b48eadd9036abd07399a7c2e996
Simon Schötz <simonsch.tz@gmail.com>
2025-08-09 14:07:08 +0200

    refactor header button for hover fx

--- components/button/variants/HeaderButton.tsx
+++ components/button/variants/HeaderButton.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { H3 } from '@/components/Headings';
 import { Icon } from '@/components/Icon';
 import { FCProps, HtmlProps } from '@/types';
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
+import { HoverBlob } from '../components';
 
 export type HeaderButtonProps = HtmlProps<'button'> & {
   config: 'header';
@@ -10,6 +13,7 @@ export type HeaderButtonProps = HtmlProps<'button'> & {
   isLoading?: boolean;
   icon?: ComponentProps<typeof Icon>['iconName'];
   flipIcon?: boolean;
+  colorVar?: string;
 };
 
 export const HeaderButton: FCProps<HeaderButtonProps> = ({
@@ -18,26 +22,61 @@ export const HeaderButton: FCProps<HeaderButtonProps> = ({
   icon,
   flipIcon,
   className = '',
+  colorVar,
   ...props
 }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const color = hovered ? colorVar ?? 'var(--fg)' : 'var(--bg)';
+
+  const textTransition = 'transition duration-600 ease-in-out';
+
   return (
     <button
       aria-label={props['aria-label'] ?? label}
-      className={`flex justify-between items-center bg-fg text-bg ${className}`}
+      className={`relative flex justify-between items-center text-bg ${className} group overflow-hidden`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
       {...props}
     >
-      <div className='flex flex-col items-start md:flex-row md:container-center md:items-center md:gap-8'>
-        <H3 className='uppercase text-3xl font-medium' text={label} />
+      <HoverBlob />
 
-        {subLabel && <span className='text-base'>{subLabel}</span>}
+      <div className='z-10 flex flex-col items-start md:flex-row md:container-center md:items-center md:gap-8'>
+        <div className='relative'>
+          <H3
+            className={`uppercase text-3xl font-medium leading-none ${textTransition}`}
+            text={label}
+            style={{ color }}
+          />
+
+          <Underline style={{ background: color }} />
+        </div>
+
+        {subLabel && (
+          <span className={`text-base ${textTransition}`} style={{ color }}>
+            {subLabel}
+          </span>
+        )}
       </div>
 
       {icon && (
         <Icon
           iconName={icon}
-          className={`w-8 h-8 ${flipIcon ? '' : 'rotate-x-180'} duration-600`}
+          style={{ color }}
+          className={`w-8 h-8 ${
+            flipIcon ? '' : 'rotate-x-180'
+          } duration-600 group-hover:scale-110`}
         />
       )}
     </button>
+  );
+};
+
+const Underline: FCProps<HtmlProps<'div'>> = ({ ...props }) => {
+  return (
+    <div
+      {...props}
+      className='absolute h-1 w-full bg-bg scale-x-0 origin-left bottom-0.5 group-hover:scale-x-100 transition duration-300 ease-in-out rounded-full'
+    />
   );
 };
