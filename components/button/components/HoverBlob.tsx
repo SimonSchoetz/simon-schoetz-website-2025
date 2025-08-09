2669245aa7a7d96f94be8a2b3335fe1a347be675
Simon Schötz <simonsch.tz@gmail.com>
2025-08-09 14:04:34 +0200

    make hover blob of button a shared component

---
+++ components/button/components/HoverBlob.tsx
@@ -0,0 +1,15 @@
+import { FCProps, HtmlProps } from '@/types';
+
+export const HoverBlob: FCProps<HtmlProps<'div'>> = ({ ...props }) => {
+  const positioning = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2';
+  const baseStyles =
+    'z-0 bg-bg w-full h-auto aspect-square rounded-full absolute transition-all duration-600 ease-in-out';
+  const transitionStart = 'scale-0';
+  const transitionEnd = 'group-hover:scale-[200%]';
+  return (
+    <div
+      {...props}
+      className={`${baseStyles} ${positioning} ${transitionStart} ${transitionEnd}`} //
+    />
+  );
+};
--- components/button/variants/DefaultButton.tsx
+++ components/button/variants/DefaultButton.tsx
@@ -3,6 +3,7 @@
 import { Icon } from '@/components';
 import { FCProps, HtmlProps } from '@/types';
 import { ComponentProps, useState } from 'react';
+import { HoverBlob } from '../components';
 
 export type DefaultButtonProps = HtmlProps<'button'> & {
   config: 'default';
@@ -22,16 +23,16 @@ export const DefaultButton: FCProps<DefaultButtonProps> = ({
   ...props
 }) => {
   const baseStyles =
-    'relative border uppercase gap-2 py-4 px-8  text-bg text-sm font-bold rounded-2xl overflow-hidden group transition-all duration-600 ease-in-out';
+    'relative border border-transparent uppercase gap-2 py-4 px-8  text-bg text-sm font-bold rounded-2xl overflow-hidden group transition-all duration-600 ease-in-out';
   const [hovered, setHovered] = useState(false);
 
   return (
     <button
       aria-label={props['aria-label'] ?? label}
-      className={`${baseStyles} ${className} test-hover`}
+      className={`${baseStyles} ${className}`}
       style={{
         background: hovered ? 'var(--bg)' : colorVar ?? 'var(--fg)',
-        borderColor: colorVar ?? 'var(--fg)',
+        borderColor: hovered ? colorVar ?? 'var(--fg)' : 'transparent',
         color: hovered ? colorVar ?? 'var(--fg)' : 'var(--bg)',
       }}
       onMouseEnter={() => setHovered(true)}
@@ -39,6 +40,7 @@ export const DefaultButton: FCProps<DefaultButtonProps> = ({
       {...props}
     >
       <HoverBlob />
+
       <span className='container-center gap-4 relative z-20'>
         {label}
         {icon && (
@@ -46,24 +48,10 @@ export const DefaultButton: FCProps<DefaultButtonProps> = ({
             iconName={icon}
             className={`${
               flipIcon ? '' : 'rotate-x-180'
-            } transition-transform duration-600`}
+            } transition-transform duration-600 group-hover:scale-110`}
           />
         )}
       </span>
     </button>
-  );
-};
-
-const HoverBlob: FCProps<HtmlProps<'div'>> = ({ ...props }) => {
-  const positioning = 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2';
-  const baseStyles =
-    'z-10 bg-bg w-full h-auto aspect-square rounded-full absolute transition-all duration-600 ease-in-out';
-  const transitionStart = 'scale-0';
-  const transitionEnd = 'group-hover:scale-[200%]';
-  return (
-    <div
-      {...props}
-      className={`${baseStyles} ${positioning} ${transitionStart} ${transitionEnd}`} // -top-[120%]
-    />
   );
 };
