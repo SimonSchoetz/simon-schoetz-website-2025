15a04eb3c4e33d8bc7cecdce917c762be467d943
Simon Schötz <simonsch.tz@gmail.com>
2025-09-01 13:17:10 +0200

    add link to footer

--- app/_sections/Footer.tsx
+++ app/_sections/Footer.tsx
@@ -1,9 +1,25 @@
 import { FooterCandy } from '@/components';
+import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className='flex justify-center w-full p-16 max-sm:p-2 relative'>
-      <p>© 2025 Simon Schötz</p>
+    <footer className='flex justify-center w-full p-16 max-sm:p-2 relative gap-8'>
+      <span>© 2025 Simon Schötz</span>
+
+      <span>·</span>
+
+      <Link
+        href='/privacy-policy'
+        className='flex gap-2 items-center hover:underline'
+      >
+        Privacy Policy
+      </Link>
+
+      <span>·</span>
+
+      <Link href='/imprint' className='flex gap-2 items-center hover:underline'>
+        Imprint
+      </Link>
 
       <FooterCandy />
     </footer>
