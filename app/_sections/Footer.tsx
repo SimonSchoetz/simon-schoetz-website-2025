3e245907b1ba7a4141965bbf75d4b164d8eb2288
Simon Schötz <simonsch.tz@gmail.com>
2025-09-10 08:09:00 +0200

    finalize footer alignment

--- app/_sections/Footer.tsx
+++ app/_sections/Footer.tsx
@@ -2,22 +2,20 @@ import { FooterCandy } from '@/components';
 import Link from 'next/link';
 
 export const Footer = () => {
+  const linkClassName = 'text-center hover:underline';
   return (
-    <footer className='flex justify-center w-full p-16 max-sm:p-2 relative gap-8'>
-      <span>© 2025 Simon Schötz</span>
+    <footer className='flex content-center justify-center text-center text-xs flex-wrap w-full p-16 section-padding relative md:gap-8 max-md:flex-col'>
+      <span>© 2025 Simon Schötz | All rights reserved</span>
 
       <span>·</span>
 
-      <Link
-        href='/privacy-policy'
-        className='flex gap-2 items-center hover:underline'
-      >
+      <Link href='/privacy-policy' className={linkClassName}>
         Privacy Policy
       </Link>
 
       <span>·</span>
 
-      <Link href='/imprint' className='flex gap-2 items-center hover:underline'>
+      <Link href='/imprint' className={linkClassName}>
         Imprint
       </Link>
 
