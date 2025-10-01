2963195e2331bbb6f74ffe1fbc887bbe87d1b9c0
Simon Schötz <simonsch.tz@gmail.com>
2025-10-01 13:29:41 +0200

    adjust imprint

--- app/(side)/imprint/page.tsx
+++ app/(side)/imprint/page.tsx
@@ -6,6 +6,9 @@ const ImprintPage: NextPage = () => {
   return (
     <main className='flex flex-col relative section-padding'>
       <H1 text='Imprint' />
+
+      <p>Information according to § 5 TMG:</p>
+
       <H2 text='Simon Schötz' className={h2ClassName} />
       <p>
         Brusendorfer Straße 14
@@ -16,14 +19,24 @@ const ImprintPage: NextPage = () => {
       <H2 text='Contact' className={h2ClassName} />
       <p>Email: info@simonschoetz.de</p>
 
-      <H2 text='Responsible for content:' className={h2ClassName} />
-      <p>Simon Schötz</p>
-
-      <H2 text='Data Protection Officer' className={h2ClassName} />
-      <p>Simon Schötz</p>
+      <H2
+        text='Responsible for content according to § 55 Abs. 2 RStV:'
+        className={h2ClassName}
+      />
+      <p>
+        Simon Schötz
+        <br />
+        Brusendorfer Straße 14
+        <br />
+        12055 Berlin, Germany
+      </p>
 
       <H2 text='Dispute resolution' className={h2ClassName} />
       <p>
+        The European Commission provides a platform for online dispute
+        resolution (ODR): https://ec.europa.eu/consumers/odr/.
+      </p>
+      <p className='mt-4'>
         I am not willing or obliged to participate in dispute resolution
         proceedings before a consumer arbitration board.
       </p>
